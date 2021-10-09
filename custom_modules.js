58ae87d08d3007a1d1687e11cfe510ae1fe12a62
ClaytonSiby <claytonsiby@gmail.com>
2021-10-09 04:55:52 +0200

    Add custom modules example

---
+++ custom_modules.js
@@ -0,0 +1,13 @@
+// Modules
+const { clayton, clement } = require('./names');
+const sayHello = require('./utils');
+const data = require('./alternative-flavor');
+require('./mind-grenade');
+
+// CommonJs, every file is module by default
+// Modules - Encapsulated Code ( Only share minimum)
+
+// sayHello('Tafadzwa');
+// sayHello(clement);
+// sayHello(clayton);
+// console.log(data);
